950da9985884f03e4a6f8c5ac1d92e069dbac6a7
zipmic <mail@code.dk>
2022-08-09 15:47:14 +0200

    Et par ting jeg skal bruge på windows

--- Assets/Scripts/LauncherScript.cs
+++ Assets/Scripts/LauncherScript.cs
@@ -12,15 +12,27 @@ public class LauncherScript : MonoBehaviour
 	private void Awake()
 	{
         Application.runInBackground = false;
-        Application.targetFrameRate = 100;
+        Application.targetFrameRate = 60;
 	}
 
 	private void Start()
 	{
+     
+        // ../ for at gå en mappe op og så ind i builds.
        path =  Application.dataPath + "/../Builds/";
+
+
+        // Check alle directory under builds
+        // Hvis der ikke er exe og billede i png så ignorer
+        // string[] folders = System.IO.Directory.GetDirectories(@"C:\My Sample Path\","*", System.IO.SearchOption.AllDirectories);
+        // tjek EXE navn
+        //string[] allfiles = Directory.GetFiles("path/to/dir", "*.exe*", SearchOption.AllDirectories);
+        // Hent png
+        // logo.png
+        // https://gyanendushekhar.com/2017/07/08/load-image-runtime-unity/
     }
 
-	public void StartProcess(string FolderName, string ExeName)
+    public void StartProcess(string FolderName, string ExeName)
     {
         print(path + FolderName + "/" + ExeName + ".exe");
         _process = Process.Start(path + FolderName + "/" + ExeName + ".exe");
